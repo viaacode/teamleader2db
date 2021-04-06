8c9dd4ff4ff6a0b1ba05e055aabed426f9fbf24a
Walter Schreppers <schreppers@gmail.com>
2021-04-06 14:45:34 +0200

    5 tests pass again

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -19,13 +19,13 @@ class TestApi:
         response = client.post("/sync/teamleader?full_sync=true")
         assert response.status_code == 200
         content = response.json()
-        assert 'AVO sync started' in content['status']
+        assert 'Teamleader sync started' in content['status']
 
     def test_teamleader_delta_sync(self, client):
         response = client.post("/sync/teamleader?full_sync=false")
         assert response.status_code == 200
         content = response.json()
-        assert 'AVO sync started' in content['status']
+        assert 'Teamleader sync started' in content['status']
 
     def test_health(self, client):
         response = client.get("/health/live")
@@ -35,7 +35,7 @@ class TestApi:
     def test_swagger_docs(self, client):
         response = client.get('/')
         content = response.text
-        assert 'Ldap2Db - Swagger UI' in content
+        assert 'Teamleader2Db - Swagger UI' in content
 
     def test_teamleader_status(self, client):
         response = client.get("/sync/teamleader")
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -4,30 +4,38 @@
 import pytest
 from unittest.mock import patch
 from datetime import datetime
-from ldap3.core.exceptions import LDAPExceptionError
 from psycopg2 import OperationalError as PSQLError
 from app.app import App
 
 
-@patch("app.app.AvoClient")
-@patch("app.app.LdapClient")
-@patch("app.app.DeeweeClient")
+@patch("app.app.TeamleaderClient")
+@patch("app.app.Companies")
+@patch("app.app.Contacts")
+@patch("app.app.Departments")
+# Events
+# Invoices
+# Projects
+# Users(db_conf, table_names)
 class TestApp:
 
-    @patch.object(App, 'deewee_sync', return_value=None)
+    @patch.object(App, 'teamleader_sync', return_value=None)
     def test_main_full(
             self,
             sync_mock,
-            deewee_client_mock,
-            ldap_client_mock,
-            avo_client_mock):
+            teamleader_client_mock,
+            companies_mock, contacts_mock, departments_mock
+    ):
         # Mock max_last_modified_timestamp to return None
-        deewee_client_mock().max_last_modified_timestamp.return_value = None
+        companies_mock().max_last_modified_timestamp.return_value = None
+        contacts_mock().max_last_modified_timestamp.return_value = None
+        departments_mock().max_last_modified_timestamp.return_value = None
         app = App()
-        app.deewee_sync_job()
+        app.teamleader_sync()
 
         assert sync_mock.call_count == 1
-        assert deewee_client_mock().max_last_modified_timestamp.call_count == 1
+        assert companies_mock().max_last_modified_timestamp.call_count == 1
+        assert contacts_mock().max_last_modified_timestamp.call_count == 1
+        assert departments_mock().max_last_modified_timestamp.call_count == 1
 
         call_arg = sync_mock.call_args[0][0]
         assert call_arg is None
@@ -51,37 +59,23 @@ class TestApp:
         call_arg = sync_mock.call_args[0][0]
         assert isinstance(call_arg, datetime)
 
-    def test_sync_deewee(
-            self,
-            deewee_client_mock,
-            ldap_client_mock,
-            avo_client_mock):
-        # Mock LdapClient to return certain orgs and persons
-        ldap_client_mock().search_orgs.return_value = [['org1']]
-        ldap_client_mock().search_people.return_value = [['person1']]
+    # def test_sync_deewee(
+    #        self,
+    #        deewee_client_mock,
+    #        ldap_client_mock,
+    #        avo_client_mock):
+    #    # Mock LdapClient to return certain orgs and persons
+    #    ldap_client_mock().search_orgs.return_value = [['org1']]
+    #    ldap_client_mock().search_people.return_value = [['person1']]
 
-        app = App()
-        app.deewee_sync_job()
+    #    app = App()
+    #    app.deewee_sync_job()
 
-        assert ldap_client_mock().search_orgs.call_count == 1
-        assert ldap_client_mock().search_people.call_count == 1
-        assert deewee_client_mock().upsert_ldap_results_many.call_count == 2
-        assert deewee_client_mock().upsert_ldap_results_many.call_args[0][0] == [
-            (['person1'], 'people')]
-
-    def test_sync_avo(
-            self,
-            deewee_client_mock,
-            ldap_client_mock,
-            avo_client_mock):
-        # Mock LdapClient to return certain orgs and persons
-        ldap_client_mock().search_orgs_and_units.return_value = [['org1']]
-
-        app = App()
-        app.avo_sync_job()
-
-        assert ldap_client_mock().search_orgs_and_units.call_count == 1
-        assert avo_client_mock().upsert_ldap_results_many.call_count == 1
+    #    assert ldap_client_mock().search_orgs.call_count == 1
+    #    assert ldap_client_mock().search_people.call_count == 1
+    #    assert deewee_client_mock().upsert_ldap_results_many.call_count == 2
+    #    assert deewee_client_mock().upsert_ldap_results_many.call_args[0][0] == [
+    #        (['person1'], 'people')]
 
     def test_argh_command_line_help(
             self,
@@ -98,12 +92,12 @@ class TestApp:
         deewee_client_mock().max_last_modified_timestamp.side_effect = PSQLError
         app = App()
         with pytest.raises(PSQLError):
-            app.deewee_sync_job()
+            app.teamleader_sync()
 
-    @patch.object(App, 'deewee_sync', side_effect=LDAPExceptionError)
+    @patch.object(App, 'deewee_sync', side_effect=SystemError)
     def test_main_ldap_error(self, sync_mock, deewee_client_mock,
                              ldap_client_mock, avo_client_mock):
         deewee_client_mock().max_last_modified_timestamp.return_value = None
         app = App()
-        with pytest.raises(LDAPExceptionError):
-            app.deewee_sync_job()
+        with pytest.raises(SystemError):
+            app.teamleader_sync()
