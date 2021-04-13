8a5e99daf245605aee2a91e57eb71965edc836a9
Walter Schreppers <schreppers@gmail.com>
2021-04-13 19:23:40 +0200

    more tests added that also test the case with 401 responses. coverage now 91%

--- tests/unit/comm/test_teamleader_client.py
+++ tests/unit/comm/test_teamleader_client.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from unittest.mock import patch, Mock  # , MagicMock
+from unittest.mock import patch, Mock, MagicMock
 from app.app import App
 
+API_URL = 'https://api.teamleader.eu'
 
-# class MockResponse:
-#     def __init__(self, code, data):
-#         self.status_code = code
-#         self.data = data
-#
-#     def json(self):
-#         return self.data
+
+class MockResponse:
+    def __init__(self, code, data):
+        self.status_code = code
+        self.data = data
+
+    def json(self):
+        return self.data
 
 
 @patch('app.app.Users')
@@ -26,32 +28,70 @@ from app.app import App
 @patch('app.comm.teamleader_client.RATE_LIMIT_SLEEP', 0)
 class TestTeamleaderClient:
 
-    # def mock_api_calls(self, *args, **kwargs):
-    #    url = args[0]
-    #    if '.list' in url:
-    #        assert args[0] == f'api_uri/{self.resource_name}.list'
-    #        page = kwargs['params']['page[number]']
-    #        if page > 1:  # only mock a single page of data
-    #            response = MockResponse(200, {'data': []})
-    #        else:
-    #            response = MockResponse(200, {'data': [{'id': 'uuid1'}]})
-    #    elif '.info' in url:
-    #        assert args[0] == f'api_uri/{self.resource_name}.info'
-    #        response = MockResponse(
-    #            200, {'data': {'id': 'uuid1', 'data': 'company data here'}})
-    #    else:
-    #        print(f"\n args={args} kwargs={kwargs}", flush=True)
+    def mock_api_calls(self, *args, **kwargs):
+        url = args[0]
+        if '.list' in url:
+            assert args[0] == f'{API_URL}/{self.resource_name}.list'
+            page = kwargs['params']['page[number]']
+            if page > 1:  # only mock a single page of data
+                return MockResponse(200, {'data': []})
+            else:
+                if self.list_unauthorized:
+                    self.list_unauthorized = False
+                    return MockResponse(401, {'error': 'access denied'})
+                else:
+                    return MockResponse(200, {'data': [{'id': 'uuid1'}]})
 
-    #    return response
+        if '.info' in url:
+            assert args[0] == f'{API_URL}/{self.resource_name}.info'
 
-    # def test_contacts_sync(self, mock_requests, mock_auth_table, *models):
-    #    ma = mock_auth_table.return_value
-    #    ma.count.return_value = 0
-    #    app = App()
+            if self.details_unauthorized:
+                self.details_unauthorized = False
+                return MockResponse(401, {'error': 'access denied'})
+            else:
+                return MockResponse(
+                    200,
+                    {'data': {'id': 'uuid1', 'data': 'resource data here'}}
+                )
 
-    #    self.resource_name = 'contacts'
-    #    mock_requests.get = MagicMock(side_effect=self.mock_api_calls)
-    #    app.contacts_sync(full_sync=True)
+        print(
+            f"ERROR UNHANDLED REQUEST \n args={args} kwargs={kwargs}",
+            flush=True
+        )
+
+    def test_unauth_listing(self, mock_requests, mock_auth_table, *models):
+        ma = mock_auth_table.return_value
+        ma.count.return_value = 0
+        app = App()
+
+        self.list_unauthorized = True
+        self.resource_name = 'contacts'
+        mock_requests.get = MagicMock(side_effect=self.mock_api_calls)
+
+        result = app.tlc.request_page(
+            f'/{self.resource_name}.list',
+            1, 20, None
+        )
+
+        assert result == [{'id': 'uuid1'}]
+        assert not self.list_unauthorized
+
+    def test_unauth_details(self, mock_requests, mock_auth_table, *models):
+        ma = mock_auth_table.return_value
+        ma.count.return_value = 0
+        app = App()
+
+        self.details_unauthorized = True
+        self.resource_name = 'contacts'
+        mock_requests.get = MagicMock(side_effect=self.mock_api_calls)
+
+        result = app.tlc.request_item(
+            f'/{self.resource_name}.info',
+            'uuid1'
+        )
+
+        assert result == {'data': 'resource data here', 'id': 'uuid1'}
+        assert not self.details_unauthorized
 
     def test_custom_fields(self, mock_requests, mock_auth_table, *models):
         ma = mock_auth_table.return_value
